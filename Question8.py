97b97da73990e05624585beddad79737c2d1ef8b
ARUN PAL <arunpal6390@gmail.com>
2022-08-30 17:33:47 +0530

    new

---
+++ Question8.py
@@ -0,0 +1,3 @@
+#8.Write a python program to sort the list alphanumerically – thislist = ["Java", "SQL","C", "Reactjs", "Javascript", "Python"]
+thislist = ["Java", "SQL","C", "Reactjs", "Javascript", "Python"]
+print(sorted(thislist))
