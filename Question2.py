97e17110724473b391dfd783d7a2fc98c78437aa
ARUN PAL <arunpal6390@gmail.com>
2022-08-30 17:25:22 +0530

    Some new type

---
+++ Question2.py
@@ -0,0 +1,12 @@
+#Write a python script to get the data type of a list.
+mylist = ["hello", "world", 1, 2, 9999, "pizza", 42, ["this", "is"], -100,9.5]
+print(type(mylist[0]))#str
+print(type(mylist[1]))#str
+print(type(mylist[2]))#int
+print(type(mylist[3]))#int
+print(type(mylist[4]))#int
+print(type(mylist[5]))#str
+print(type(mylist[6]))#int
+print(type(mylist[7]))#list
+print(type(mylist[8]))#int
+print(type(mylist[9]))#float
