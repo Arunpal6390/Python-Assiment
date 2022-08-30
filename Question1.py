0e0eb4865efd67696efa2ec29b9cd733b8382055
ARUN PAL <arunpal6390@gmail.com>
2022-08-30 17:21:55 +0530

    first commit

---
+++ Question1.py
@@ -0,0 +1,5 @@
+#list
+#Write a python script to store multiple items in a single variable ( Items are “Java”,“Python”, “SQL”, “C” ) using list
+list=["Java","Python", "SQL", "C"]
+print(list)
+print(type(list))
