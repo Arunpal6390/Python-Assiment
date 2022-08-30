7781fe32e66634d099cd022e93ea9a900940ae59
ARUN PAL <arunpal6390@gmail.com>
2022-08-30 17:30:16 +0530

    New

---
+++ Question4.py
@@ -0,0 +1,6 @@
+#Write a python script to Change the values "SQL" and "Reactnative" with the values "NoSQL" and "Flutter" (List is thislist = ["Java", "SQL", "C", "Reactnative",
+#"Javascript", "Python"]
+l3 = ["Java", "SQL", "C", "Reactnative","Javascript", "Python"]
+l3[1]="NoSQL"
+l3[3]="Flutter"
+print((l3))
