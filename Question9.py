aad076a0592d40e4619e0ad6c674d8095981d50d
ARUN PAL <arunpal6390@gmail.com>
2022-08-30 17:34:36 +0530

    New

---
+++ Question9.py
@@ -0,0 +1,5 @@
+#9.Write a Python script to create a list of city names taken from the user.
+# For list of string 
+lst1 = []  
+lst1 = [city for city in input("Enter the list city : ").split()]
+print(lst1)
