3b3c23137e32d0e5fda915a29928deb6356c73d9
ARUN PAL <arunpal6390@gmail.com>
2022-08-30 17:32:10 +0530

    new

---
+++ Question6.py
@@ -0,0 +1,8 @@
+#6.Write a python program to append elements from another list to the current list.(firstlist = ["Java", "Python", "SQL"] secondlist = ["C", "Cpp", "NoSQL"] ), "NoSQL"] )
+l1=[22,44,66,87,88]
+firstlist = ["Java", "Python", "SQL"]
+secondlist = ["C", "Cpp", "NoSQL"] 
+firstlist.append(88)
+secondlist.append(66)
+print("First list is:", firstlist)
+print("Second list is:" ,secondlist)
