b88b0f77d371165f00e5c8f82d49f0a4e6065b27
ARUN PAL <arunpal6390@gmail.com>
2022-08-30 17:32:57 +0530

    new

---
+++ Question7.py
@@ -0,0 +1,3 @@
+#7.Write a python program to Print all items by referring to their index number (thislist =["Java", "SQL", "C", "Reactnative", "Javascript", "Python"]
+thislist =["0 - Java", "1 - SQL", "2 - C", "3 - Reactnative", "4 - Javascript", "5 - Python"]
+print(thislist)
