8f30dbac4ddeba1ba486ccd42f51a7f34f79b734
ARUN PAL <arunpal6390@gmail.com>
2022-08-30 17:31:14 +0530

    new

---
+++ Question5.py
@@ -0,0 +1,4 @@
+#5.Write a python script to add an item to the end of the list (item “Python”. (mylist ["Java", "SQL", "C", "Reactnative"]
+lis2=["Java", "SQL", "C", "Reactnative"]
+lis2.append("Python")
+print(lis2)
